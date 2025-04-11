1e633b42dd41235d03fd48008f756f7a96274d66
Bhadri Narayanan <narayananbhadri34@gmail.com>
2025-04-11 19:32:30 +0530

    a new branch

--- hunger.py
+++ hunger.py
@@ -2,3 +2,9 @@ hunger=input("Are you ready for it ")
 print("Are you ready for it? ", hunger)
 if hunger=="yes":  
     print("Let's eat!")
+else:
+    thirsty=input("are you thirsty?")
+    if thirsty=="yes":
+        print("Let's drink!")
+    else:
+        print("Let's sleep!")
